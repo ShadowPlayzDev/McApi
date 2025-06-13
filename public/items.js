fe38ef4f170e451941eb66bf7d0c94e016a51f7d
Steve <149971541+ShadowPlayzDev@users.noreply.github.com>
2025-06-13 11:50:50 -0500

    Create items.js

---
+++ public/items.js
@@ -0,0 +1,17 @@
+// Array of Mcdonald's Item's
+const mcDonaldsItems = [
+    "chicken_nugget",
+    "big_mac",
+    "fries",
+    "mcflurry",
+    "cheeseburger",
+    "quarter_pounder",
+    "egg_mcmuffin",
+    "hamburger",
+    "double_cheeseburger",
+    "fillet_o_fish",
+    "hot_fudge_sundae",
+    "apple_pie"
+];
+
+export default mcDonaldsItems;
